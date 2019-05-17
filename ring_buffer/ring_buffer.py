c7ff92e7b4954356729ec667161d0b5ad9d854db
amberm <nekosenpai@gmail.com>
2019-05-17 09:15:29 -0600

    working through ring append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,8 +4,45 @@ class RingBuffer:
     self.current = 0
     self.storage = [None]*capacity
 
+
   def append(self, item):
-    pass
+    if self.current > self.capacity:
+      self.current = self.current +1
+
+      self.storage.append(item)
+
+    if self.current == self.capacity:
+      #del self.storage[]
+
+      self.current = self.current +1
+
+      self.storage.append(item)
+
+
 
   def get(self):
     pass
+
+
+
+# -----------------------------------------
+
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+buffer.get()   # should return ['d', 'e', 'f']
